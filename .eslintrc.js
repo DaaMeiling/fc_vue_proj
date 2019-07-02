d40c4b79c0bd7b17c6ae6f30862bad8982dc13f9
003121 <zhaomeiling@fcbox.com>
2019-07-02 14:25:00 +0800

    ci: 修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,25 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+		es6: true,
+		node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+	extends: ["plugin:vue/essential", "@vue/prettier","eslint:recommended"],
+	parserOptions: {
+		ecmaVersion: 2015,
+		sourceType: "module",
+		parser: "babel-eslint"
+	},
+	plugins: ['vue', 'prettier'],
   rules: {
+		"prettier/prettier": "error",
+		"semi": "error",
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
     parser: "babel-eslint"
-  }
+	},
+	overrides: []
 };
